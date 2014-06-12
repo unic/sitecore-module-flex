e4e7fb3c174675e9424a6b2fac657b958f01afbf
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2014-06-12 15:48:31 +0200

    Add todo for strange behavior on email field validation.

--- src/Unic.Flex.Implementation/Fields/InputFields/EmailField.cs
+++ src/Unic.Flex.Implementation/Fields/InputFields/EmailField.cs
@@ -15,6 +15,7 @@
         /// </summary>
         public EmailField()
         {
+            // todo: this validator shows two different validation messages on client side validation -> is 1 coming from jquery validation and one from the regex validator?
             this.DefaultValidators.Add(new EmailValidator { ValidationMessage = EmailValidator.ValidationMessageDictionaryKey });
         }
         
