e7c6cfee2eaa98f4746f744e9f69b744e3e2066b
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2014-06-12 10:19:02 +0200

    Add email validator.

---
+++ src/Unic.Flex.Implementation/Validators/EmailValidator.cs
@@ -0,0 +1,40 @@
+﻿namespace Unic.Flex.Implementation.Validators
+{
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using Unic.Flex.Model.GlassExtensions.Attributes;
+
+    /// <summary>
+    /// Regular expression validator to validate an email address.
+    /// </summary>
+    [SitecoreType(TemplateId = "{D9FC76E6-3C1B-44EE-9C91-51E97428153A}")]
+    public class EmailValidator : RegularExpressionValidator
+    {
+        /// <summary>
+        /// The validation message dictionary key
+        /// </summary>
+        public new const string ValidationMessageDictionaryKey = "Email is invalid";
+
+        /// <summary>
+        /// Gets or sets the validation message.
+        /// </summary>
+        /// <value>
+        /// The validation message.
+        /// </value>
+        [SitecoreDictionaryFallbackField("Validation Message", ValidationMessageDictionaryKey)]
+        public override string ValidationMessage { get; set; }
+
+        /// <summary>
+        /// Gets the regular expression.
+        /// </summary>
+        /// <value>
+        /// The regular expression.
+        /// </value>
+        public override string RegularExpression
+        {
+            get
+            {
+                return @"^((([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+(\.([a-z]|\d|[!#\$%&'\*\+\-\/=\?\^_`{\|}~]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])+)*)|((\x22)((((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(([\x01-\x08\x0b\x0c\x0e-\x1f\x7f]|\x21|[\x23-\x5b]|[\x5d-\x7e]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(\\([\x01-\x09\x0b\x0c\x0d-\x7f]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF]))))*(((\x20|\x09)*(\x0d\x0a))?(\x20|\x09)+)?(\x22)))@((([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|\d|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.)+(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])|(([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])([a-z]|\d|-|\.|_|~|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])*([a-z]|[\u00A0-\uD7FF\uF900-\uFDCF\uFDF0-\uFFEF])))\.?$";
+            }
+        }
+    }
+}
