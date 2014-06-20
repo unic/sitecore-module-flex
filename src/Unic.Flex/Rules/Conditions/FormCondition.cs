30ac9e9b1422cc7738f868f312008320fd70f31a
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2014-06-20 09:40:50 +0200

    Add conditions for the configuration module ("True", "Web Site Name", "Device Name", "Form Id" and "Instance Name").

---
+++ src/Unic.Flex/Rules/Conditions/FormCondition.cs
@@ -0,0 +1,48 @@
+﻿namespace Unic.Flex.Rules.Conditions
+{
+    using Sitecore.Data;
+    using Sitecore.Diagnostics;
+    using Sitecore.Rules;
+    using Sitecore.Rules.Conditions;
+    using Unic.Flex.Context;
+
+    /// <summary>
+    /// Conditional rule to validate the current form.
+    /// </summary>
+    /// <typeparam name="T">Generic parameter</typeparam>
+    public class FormCondition<T> : WhenCondition<T> where T : RuleContext
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FormCondition{T}"/> class.
+        /// </summary>
+        public FormCondition()
+        {
+            this.ItemId = ID.Null;
+        }
+
+        /// <summary>
+        /// Gets or sets the item id.
+        /// </summary>
+        /// <value>
+        /// The item id.
+        /// </value>
+        public ID ItemId { get; set; }
+
+        /// <summary>
+        /// Executes the specified rule context.
+        /// </summary>
+        /// <param name="ruleContext">The rule context.</param>
+        /// <returns>Boolean value wheater the condition is true/false</returns>
+        protected override bool Execute(T ruleContext)
+        {
+            Assert.ArgumentNotNull(ruleContext, "ruleContext");
+
+            // get the current form
+            var formContext = FlexContext.Current.Form;
+            if (formContext == null) return false;
+
+            // compare the id's
+            return new ID(formContext.Id) == this.ItemId;
+        }
+    }
+}
