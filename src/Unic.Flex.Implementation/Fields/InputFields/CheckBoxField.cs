97d6a9b26b886a77bb437681d07c3411a13f2210
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2014-06-23 11:09:03 +0200

    Override the checkbox TextValue and return either "Yes" or "No".

--- src/Unic.Flex.Implementation/Fields/InputFields/CheckBoxField.cs
+++ src/Unic.Flex.Implementation/Fields/InputFields/CheckBoxField.cs
@@ -1,6 +1,9 @@
 ﻿namespace Unic.Flex.Implementation.Fields.InputFields
 {
     using Glass.Mapper.Sc.Configuration.Attributes;
+    using Ninject;
+    using Unic.Flex.DependencyInjection;
+    using Unic.Flex.Globalization;
     using Unic.Flex.Model.DomainModel.Fields.InputFields;
 
     /// <summary>
@@ -9,6 +12,37 @@
     [SitecoreType(TemplateId = "{8583A797-3FE5-4247-B190-26E9BFD4D5A4}")]
     public class CheckBoxField : InputField<bool>
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="CheckBoxField"/> class.
+        /// </summary>
+        public CheckBoxField()
+        {
+            Container.Kernel.Inject(this);
+        }
+
+        /// <summary>
+        /// Gets or sets the dictionary repository.
+        /// </summary>
+        /// <value>
+        /// The dictionary repository.
+        /// </value>
+        [Inject]
+        public IDictionaryRepository DictionaryRepository { private get; set; }
+
+        /// <summary>
+        /// Gets the text value.
+        /// </summary>
+        /// <value>
+        /// The text value.
+        /// </value>
+        public override string TextValue
+        {
+            get
+            {
+                return this.Value ? this.DictionaryRepository.GetText("Yes") : this.DictionaryRepository.GetText("No");
+            }
+        }
+
         /// <summary>
         /// Gets the name of the view.
         /// </summary>
