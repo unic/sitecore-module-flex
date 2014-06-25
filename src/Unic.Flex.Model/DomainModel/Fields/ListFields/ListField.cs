d9a44dfc106edf0b2fb893a534c458f4d8373bce
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2014-06-25 08:58:15 +0200

    Rewrite TextValue for list fields to get comma separated list of values.

--- src/Unic.Flex.Model/DomainModel/Fields/ListFields/ListField.cs
+++ src/Unic.Flex.Model/DomainModel/Fields/ListFields/ListField.cs
@@ -1,6 +1,7 @@
 ﻿namespace Unic.Flex.Model.DomainModel.Fields.ListFields
 {
     using System.Collections.Generic;
+    using System.Linq;
     using System.Web.Mvc;
 
     public abstract class ListField<TValue> : FieldBase<TValue>
@@ -11,5 +12,36 @@
         }
         
         public IList<SelectListItem> Items { get; private set; }
+
+        public override string TextValue
+        {
+            get
+            {
+                var value = string.Empty;
+                
+                // check the current value
+                if (Equals(this.Value, null)) return base.TextValue;
+                if (typeof(TValue) == typeof(string[])) value = this.GetTextValue(this.Value as string[]);
+                if (typeof(TValue) == typeof(string)) value = this.GetTextValue(this.Value as string);
+                
+                // return
+                return !string.IsNullOrWhiteSpace(value) ? value : base.TextValue;
+            }
+        }
+
+        private string GetTextValue(string value)
+        {
+            var selectedItem = this.Items.FirstOrDefault(item => item.Value == value);
+            return selectedItem != null ? selectedItem.Text : string.Empty;
+        }
+
+        private string GetTextValue(IEnumerable<string> value)
+        {
+            return string.Join(
+                ", ",
+                this.Items.Where(item => !string.IsNullOrWhiteSpace(item.Value))
+                    .Where(item => value.Contains(item.Value))
+                    .Select(item => item.Text));
+        }
     }
 }
