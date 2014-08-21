174122d5d0fb2ea9505867763112d02425ba26ae
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2014-08-21 07:49:53 +0200

    Add password field and strength validator

---
+++ src/Unic.Flex.Implementation/Fields/InputFields/PasswordField.cs
@@ -0,0 +1,24 @@
+﻿namespace Unic.Flex.Implementation.Fields.InputFields
+{
+    using System;
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using Sitecore.Configuration;
+    using Unic.Flex.Implementation.Validators;
+    using Unic.Flex.Model.DomainModel.Fields.InputFields;
+
+    /// <summary>
+    /// Password field
+    /// </summary>
+    [SitecoreType(TemplateId = "{47632CE8-1AA6-4A08-8A0B-528CBAE8C07E}")]
+    public class PasswordField : InputField<string>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="PasswordField"/> class.
+        /// </summary>
+        public PasswordField()
+        {
+            // we use the standard values item here as validator id, because we do not have a specific item for the validator as it is defined here and not in Sitecore
+            this.DefaultValidators.Add(new PasswordStrengthValidator { ValidatorId = new Guid(Settings.GetSetting("Flex.PasswordStrengthValidator.StandardValues.ItemId")) });
+        }
+    }
+}
