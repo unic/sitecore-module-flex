0767c350e938f7b98ef2b5592d76369aaa10e735
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2014-09-03 12:38:57 +0200

    POSTWEPP-1565: Add initial send email plug and configuration

---
+++ src/Unic.Flex.Implementation/Configuration/SendEmailPlugConfiguration.cs
@@ -0,0 +1,10 @@
+﻿namespace Unic.Flex.Implementation.Configuration
+{
+    using Unic.Configuration;
+
+    public class SendEmailPlugConfiguration : AbstractConfiguration
+    {
+        [Configuration(FieldName = "From")]
+        public string From { get; set; }
+    }
+}
---
+++ src/Unic.Flex.Implementation/Plugs/SavePlugs/SendEmail.cs
@@ -0,0 +1,25 @@
+﻿namespace Unic.Flex.Implementation.Plugs.SavePlugs
+{
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using Sitecore.Diagnostics;
+    using Unic.Configuration;
+    using Unic.Flex.DependencyInjection;
+    using Unic.Flex.Implementation.Configuration;
+    using Unic.Flex.Model.DomainModel.Forms;
+    using Unic.Flex.Model.DomainModel.Plugs.SavePlugs;
+
+    [SitecoreType(TemplateId = "{DF5C2C2F-4A48-4206-9DFB-0DCDE27E2233}")]
+    public class SendEmail : SavePlugBase
+    {
+        public IConfigurationManager ConfigurationManager { private get; set; }
+        
+        public override void Execute(Form form)
+        {
+            Assert.ArgumentNotNull(form, "form");
+
+            this.ConfigurationManager = Container.Resolve<IConfigurationManager>();
+
+            Log.Error("---------- " + this.ConfigurationManager.Get<SendEmailPlugConfiguration>(c => c.From), this);
+        }
+    }
+}
