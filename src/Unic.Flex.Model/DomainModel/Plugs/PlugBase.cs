097d12562147c533d5c3e3445ab300997b17ec11
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2014-09-05 15:59:14 +0200

    Change todo

--- src/Unic.Flex.Model/DomainModel/Plugs/PlugBase.cs
+++ src/Unic.Flex.Model/DomainModel/Plugs/PlugBase.cs
@@ -7,7 +7,7 @@
     /// </summary>
     public abstract class PlugBase : ItemBase
     {
-        // todo: remove this message here and move it to the form -> 1 error message per form
+        //// todo: remove this message here and move it to the form -> 1 error message per form -> discuss this with Chris before implementing
         
         /// <summary>
         /// Gets or sets the error message.
