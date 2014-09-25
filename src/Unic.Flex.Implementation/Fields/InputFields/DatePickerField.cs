73a978224de17a8901937b5a9a683ee68ed41d2e
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2014-09-25 17:18:01 +0200

    POSTWEPP-2640: Add initial version of datepicker

---
+++ src/Unic.Flex.Implementation/Fields/InputFields/DatePickerField.cs
@@ -0,0 +1,31 @@
+﻿namespace Unic.Flex.Implementation.Fields.InputFields
+{
+    using System;
+    using Glass.Mapper.Sc.Configuration.Attributes;
+    using Unic.Flex.Implementation.Validators;
+    using Unic.Flex.Model.DomainModel.Fields.InputFields;
+
+    /// <summary>
+    /// Field for a date picker
+    /// </summary>
+    [SitecoreType(TemplateId = "{5CBCDE45-F0A9-4F6D-8273-685249ABDDF6}")]
+    public class DatePickerField : InputField<DateTime?>
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DatePickerField"/> class.
+        /// </summary>
+        public DatePickerField()
+        {
+            this.DefaultValidators.Add(new DateValidator());
+        }
+        
+        /// <summary>
+        /// Gets or sets the default value.
+        /// </summary>
+        /// <value>
+        /// The default value.
+        /// </value>
+        [SitecoreField("Default Value")]
+        public override DateTime? DefaultValue { get; set; }
+    }
+}
---
+++ src/Unic.Flex.Implementation/Fields/InputFields/DatePickerFieldViewModel.cs
@@ -0,0 +1,66 @@
+﻿namespace Unic.Flex.Implementation.Fields.InputFields
+{
+    using System;
+    using System.Threading;
+    using Unic.Flex.Model.ViewModel.Fields.InputFields;
+
+    /// <summary>
+    /// View model for the date picker field
+    /// </summary>
+    public class DatePickerFieldViewModel : InputFieldViewModel<DateTime?>
+    {
+        /// <summary>
+        /// The date format
+        /// </summary>
+        private readonly string dateFormat;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DatePickerFieldViewModel"/> class.
+        /// </summary>
+        public DatePickerFieldViewModel()
+        {
+            this.dateFormat = Thread.CurrentThread.CurrentCulture.DateTimeFormat.ShortDatePattern;
+        }
+        
+        /// <summary>
+        /// Gets the name of the view.
+        /// </summary>
+        /// <value>
+        /// The name of the view.
+        /// </value>
+        public override string ViewName
+        {
+            get
+            {
+                return "Fields/InputFields/DatePicker";
+            }
+        }
+
+        /// <summary>
+        /// Gets the date format.
+        /// </summary>
+        /// <value>
+        /// The date format.
+        /// </value>
+        public virtual string DateFormat
+        {
+            get
+            {
+                return "{0:" + this.dateFormat + "}";
+            }
+        }
+
+        /// <summary>
+        /// Binds the needed attributes and properties after converting from domain model to the view model
+        /// </summary>
+        public override void BindProperties()
+        {
+            base.BindProperties();
+
+            this.AddCssClass("flex_datefield");
+
+            this.Attributes.Add("aria-multiline", false);
+            this.Attributes.Add("role", "textbox");
+        }
+    }
+}
