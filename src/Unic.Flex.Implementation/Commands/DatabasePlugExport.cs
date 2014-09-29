408bd55e6e0df13d56b8b42e50d80d19334edc9f
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2014-09-29 15:07:54 +0200

    POSTWEPP-1569: Add ribbon/button for data export (without functionality).

---
+++ src/Unic.Flex.Implementation/Commands/DatabasePlugExport.cs
@@ -0,0 +1,12 @@
+﻿namespace Unic.Flex.Implementation.Commands
+{
+    using Sitecore.Shell.Framework.Commands;
+
+    public class DatabasePlugExport : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            Sitecore.Diagnostics.Log.Error("***** Database Export called", this);
+        }
+    }
+}
