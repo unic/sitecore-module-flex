15ecd421448b439c5bb24f7c1a3ee4f416b19054
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2014-11-25 14:38:59 +0100

    Add checkbox to configure if an empty option should be added to the dropdown.

--- src/Unic.Flex.Implementation/Fields/ListFields/DropDownListField.cs
+++ src/Unic.Flex.Implementation/Fields/ListFields/DropDownListField.cs
@@ -1,7 +1,10 @@
 ﻿namespace Unic.Flex.Implementation.Fields.ListFields
 {
+    using System.Collections.Generic;
     using System.Linq;
     using Glass.Mapper.Sc.Configuration.Attributes;
+    using Unic.Flex.Globalization;
+    using Unic.Flex.Model.DataProviders;
     using Unic.Flex.Model.DomainModel.Fields.ListFields;
 
     /// <summary>
@@ -10,6 +13,35 @@
     [SitecoreType(TemplateId = "{18C0BDC1-5162-4CE4-A92A-0C9A8CAFCF11}")]
     public class DropDownListField : ListField<string>
     {
+        /// <summary>
+        /// Gets or sets a value indicating whether to add an empty option to the list.
+        /// </summary>
+        /// <value>
+        ///   <c>true</c> if an empty option should be added; otherwise, <c>false</c>.
+        /// </value>
+        [SitecoreField("Add Empty Option")]
+        public virtual bool AddEmptyOption { get; set; }
+
+        /// <summary>
+        /// Gets the items.
+        /// </summary>
+        /// <value>
+        /// The items.
+        /// </value>
+        public override IList<ListItem> Items
+        {
+            get
+            {
+                var items = base.Items;
+                if (this.AddEmptyOption)
+                {
+                    items.Insert(0, new ListItem { Text = TranslationHelper.FlexText("Please choose"), Value = string.Empty });
+                }
+
+                return items;
+            }
+        }
+
         /// <summary>
         /// Gets the default value.
         /// </summary>
