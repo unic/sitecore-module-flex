ca444dfa9a1cc5bfc3e75861b37b275cab54fb29
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2015-02-27 12:04:42 +0100

    Add basic list control with static date return.

---
+++ src/Unic.Flex.Website/Controllers/FlexSpeakController.cs
@@ -0,0 +1,50 @@
+﻿namespace Unic.Flex.Website.Controllers
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+    using System.Web.Mvc;
+    using Newtonsoft.Json;
+    using System;
+
+    public class FlexSpeakController : Controller
+    {
+        public ActionResult GetAsyncTasks()
+        {
+            var tasks = new AsyncTaks();
+            tasks.Data.Add(new AsyncTask { Form = "my Form", Plug = "plug", Attemps = 4, LastTry = DateTime.Now.ToString("G") });
+            tasks.Data.Add(new AsyncTask { Form = "second", Plug = "plug", Attemps = 2, LastTry = DateTime.Now.ToString("G") });
+
+            this.Response.ContentType = "application/json";
+            
+            return this.Content(JsonConvert.SerializeObject(tasks));
+        }
+
+        [DataContract]
+        private class AsyncTaks
+        {
+            public AsyncTaks()
+            {
+                this.Data = new List<AsyncTask>();
+            }
+
+            [DataMember(Name = "data", EmitDefaultValue = false)]
+            public IList<AsyncTask> Data { get; private set; } 
+        }
+
+        [DataContract]
+        private class AsyncTask
+        {
+            [DataMember(Name = "form", EmitDefaultValue = false)]
+            public string Form { get; set; }
+
+            [DataMember(Name = "plug", EmitDefaultValue = false)]
+            public string Plug { get; set; }
+
+            [DataMember(Name = "attemps", EmitDefaultValue = false)]
+            public int Attemps { get; set; }
+
+            [DataMember(Name = "last_try", EmitDefaultValue = false)]
+            public string LastTry { get; set; }
+        }
+    }
+}
---
+++ src/Unic.Flex.Website/sitecore/shell/client/YourApps/Flex/AsyncPlugs.js
@@ -0,0 +1,17 @@
+﻿define(["sitecore"], function (Sitecore) {
+  var AsyncPlugs = Sitecore.Definitions.App.extend({
+      initialized: function () {
+
+          this.refresh();
+      },
+
+      refresh: function () {
+
+          var apiUrl = "/api/sitecore/FlexSpeak/GetAsyncTasks";
+          var requestOptions = { parameters: "", onSuccess: null, url: apiUrl };
+          this.Tasks.viewModel.getData(requestOptions);
+      }
+  });
+
+  return AsyncPlugs;
+});
