1ea9821b78ecfa1ef6509547c6cf664a03292f63
Kevin Brechbühl <kevin.brechbuehl@unic.com>
2015-07-02 14:20:47 +0200

    Add serialization to NuGet package and make it possible to install the items over NuGet

---
+++ tools/uninstall.ps1
@@ -0,0 +1,42 @@
+﻿param($installPath, $toolsPath, $package, $project)
+
+if((Get-Module Scoop) -and ((Get-Module scoop | select -first 1).Path)) {
+    $scoopPath = (Get-Module scoop | select -first 1).Path
+    $configPath =  Join-path (Split-path $scoopPath) "..\packages\Unic.Bob.Config\tools\BobConfig"
+    if($configPath) {
+        Import-Module $configPath
+
+        $config = Get-ScProjectConfig
+    }
+    else {
+        Write-Warning "BobConfig not found."
+    }
+}
+else {
+    Write-Warning "Scoop not found."
+}
+
+if($config -and $config.SerializationPath) {
+    $serializationPath = Join-Path $config.WebsitePath $config.SerializationPath
+    $appSerializationPath = Join-Path $serializationPath "app"
+    $sourceSerialization = (Join-Path $installPath "serialization") + "\"
+
+    ls $sourceSerialization -Recurse |  ? { ! $_.PSIsContainer } | % {
+        $relativePath = $_.FullName.Substring($sourceSerialization.Length)
+        $target = Join-Path $appSerializationPath $relativePath
+        if(Test-Path $target) {
+            # http://ss64.com/nt/fc.html
+            FC.EXE $target $_.FullName | Out-Null
+            if($LASTEXITCODE -eq 0) {
+                Write-Host "Remove $target"
+                rm $target
+            }
+            else {
+                Write-Warning "$target and $( $_.FullName) are different. Skipping."
+            }
+        }
+    }
+}
+else {
+    Write-Warning "No SerializationPath found in the Bob.config."
+}
