cd6b9053c6205d5ec0342ccc73160f5289bd828d
Pascal Mathys <pascal.mathys@unic.com>
2017-07-10 17:35:42 +0200

    POSTSC-1334 Improve database query performance
    - Improve performance of SaveToDatabase plug
    - Improve performance of command state check on "Export Data"  button
    - Improve performance of Excel export

--- src/Unic.Flex.Core/Database/UnitOfWork.cs
+++ src/Unic.Flex.Core/Database/UnitOfWork.cs
@@ -3,107 +3,44 @@
     using System;
     using Unic.Flex.Model.Entities;
 
-    /// <summary>
-    /// Implementing unit of work for accessing database entities
-    /// </summary>
     public class UnitOfWork : IUnitOfWork
     {
-        /// <summary>
-        /// The database context
-        /// </summary>
         private readonly FlexDbContext context;
         
-        /// <summary>
-        /// Disposed member.
-        /// </summary>
         private bool disposed;
 
-        /// <summary>
-        /// The form repository
-        /// </summary>
         private IDatabaseRepository<Form> formRepository;
 
-        /// <summary>
-        /// The job repository
-        /// </summary>
+        private IDatabaseRepository<Session> sessionRepository;
+
         private IDatabaseRepository<Job> jobRepository;
 
-        /// <summary>
-        /// The task repository
-        /// </summary>
         private IDatabaseRepository<Task> taskRepository; 
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="UnitOfWork"/> class.
-        /// </summary>
         public UnitOfWork()
         {
             this.context = new FlexDbContext();
         }
 
-        /// <summary>
-        /// Gets the form repository.
-        /// </summary>
-        /// <value>
-        /// The form repository.
-        /// </value>
-        public virtual IDatabaseRepository<Form> FormRepository
-        {
-            get
-            {
-                return this.formRepository ?? (this.formRepository = new DatabaseRepository<Form>(this.context));
-            }
-        }
+        public virtual IDatabaseRepository<Form> FormRepository => this.formRepository ?? (this.formRepository = new DatabaseRepository<Form>(this.context));
 
-        /// <summary>
-        /// Gets the jobs repository.
-        /// </summary>
-        /// <value>
-        /// The jobs repository.
-        /// </value>
-        public virtual IDatabaseRepository<Job> JobRepository
-        {
-            get
-            {
-                return this.jobRepository ?? (this.jobRepository = new DatabaseRepository<Job>(this.context));
-            }
-        }
+        public virtual IDatabaseRepository<Session> SessionRepository => this.sessionRepository ?? (this.sessionRepository = new DatabaseRepository<Session>(this.context));
 
-        /// <summary>
-        /// Gets the task repository.
-        /// </summary>
-        /// <value>
-        /// The task repository.
-        /// </value>
-        public virtual IDatabaseRepository<Task> TaskRepository
-        {
-            get
-            {
-                return this.taskRepository ?? (this.taskRepository = new DatabaseRepository<Task>(this.context));
-            }
-        }
+        public virtual IDatabaseRepository<Job> JobRepository => this.jobRepository ?? (this.jobRepository = new DatabaseRepository<Job>(this.context));
 
-        /// <summary>
-        /// Saves changes to the data provider.
-        /// </summary>
+        public virtual IDatabaseRepository<Task> TaskRepository => this.taskRepository ?? (this.taskRepository = new DatabaseRepository<Task>(this.context));
+
         public virtual void Save()
         {
             this.context.SaveChanges();
         }
 
-        /// <summary>
-        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
-        /// </summary>
         public virtual void Dispose()
         {
             this.Dispose(true);
             GC.SuppressFinalize(this);
         }
 
-        /// <summary>
-        /// Releases unmanaged and - optionally - managed resources
-        /// </summary>
-        /// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
         protected virtual void Dispose(bool disposing)
         {
             if (!this.disposed)
