5d06b57a162516b5484d223b5f6dce9f5bfd868f
CORP\alina.drescher <alina.drescher@unic.com>
2018-03-07 11:43:34 +0100

    POSTZI-825 add new saverule for saving the language

---
+++ src/Unic.Flex.Implementation/Rules/SaveRules/SaveLanguageAction.cs
@@ -0,0 +1,22 @@
+﻿namespace Unic.Flex.Implementation.Rules.SaveRules
+{
+    using System;
+    using Core.DependencyInjection;
+    using Core.MarketingAutomation;
+    using Sitecore.Rules;
+
+    public class SaveLanguageAction<T> : BaseFlexSaveAction<T>
+        where T : RuleContext
+    {
+        private readonly Guid languageField = Guid.Parse("{20076F81-D4D9-43FF-B79A-A255A3F8A9DF}");
+
+        public override void Apply(T ruleContext)
+        {
+            var analyticsContactService = DependencyResolver.Resolve<IMarketingAutomationContactService>();
+
+            var language = Sitecore.Context.Language.Name;
+
+            analyticsContactService.SetContactValue(this.languageField, language);
+        }
+    }
+}
