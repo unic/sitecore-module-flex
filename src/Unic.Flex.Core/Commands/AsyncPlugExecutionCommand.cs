d2d0de1a48860235403eb3b69d9581457b98864d
rafal.speruda <rafal.speruda@unic.com>
2020-05-20 16:58:18 +0200

    async-mail-plug Added task schedule and command

---
+++ src/Unic.Flex.Core/Commands/AsyncPlugExecutionCommand.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unic.Flex.Core.Commands
+{
+    using Sitecore.Shell.Framework.Commands;
+
+    public class AsyncPlugExecutionCommand : Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            var fields = context.Items[0].Template.Fields;
+        }
+    }
+}
